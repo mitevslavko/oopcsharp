e852f06a5777bad1478b3657b02e11514673b22c
mitevslavko <149831133+mitevslavko@users.noreply.github.com>
2024-02-28 17:55:13 +0100

    stick man

---
+++ class01/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   *");
+            Console.WriteLine("   *");
+            Console.WriteLine(" * * *");
+            Console.WriteLine("*  *  *");
+            Console.WriteLine("   *");
+            Console.WriteLine("  ***");
+            Console.WriteLine(" *   *");
+            Console.WriteLine(" *   *");
+            Console.ReadLine();
+        }
+    }
+}
